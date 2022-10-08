a05aecded9b01eff2a52f99307899a10be8663ea
Zancrow20 <ildar1qweas@gmail.com>
2022-10-08 22:55:00 +0300

    1.Calculator.fs and Parser.fs done 2.Program.fs done

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -2,6 +2,7 @@
 
 open System
 open Hw4.Calculator
+open Microsoft.FSharp.Core
 
 
 type CalcOptions = {
@@ -11,10 +12,30 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    match Array.length args with
+    | 3 -> true
+    | _ -> false
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> CalculatorOperation.Undefined
+    
+let tryParseValues (arg1 : string, arg2 : string) =
+    match Double.TryParse arg1, Double.TryParse arg2 with
+    | (true,_),(true,_) -> true
+    | _ -> false
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    match isArgLengthSupported args with
+    | false -> ArgumentException() |> raise
+    | true ->
+        match parseOperation args[1] with
+        | CalculatorOperation.Undefined -> ArgumentException() |> raise
+        | CalculatorOperation.Plus | CalculatorOperation.Minus | CalculatorOperation.Multiply | CalculatorOperation.Divide ->
+            match tryParseValues (args[0], args[2]) with
+            | false -> ArgumentException() |> raise
+            | true -> {arg1 = Double.Parse args[0]; arg2 = Double.Parse args[2]; operation = parseOperation args[1]}
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,10 @@
-﻿raise (System.NotImplementedException())
+﻿module Hw4.Program
+
+open Hw4.Calculator
+open Hw4.Parser
+
+[<EntryPoint>]
+let main args =
+    let parsedArgs = parseCalcArguments args
+    printf $"{calculate parsedArgs.arg1 parsedArgs.operation parsedArgs.arg2}"
+    0
