b735a60461eb97437517649c111925a411e4d8d4
Zancrow20 <ildar1qweas@gmail.com>
2022-10-24 16:12:42 +0300

    1.Move calculatorHadler to Logic.fs 2.Exclude Program.fs by coverlet.runsetting

---
+++ Homework6/Hw6/Logic.fs
@@ -0,0 +1,28 @@
+module Hw6.Logic
+open Microsoft.AspNetCore.Http
+open Giraffe
+open Hw6.Calculator
+open Hw6.Parser
+open Hw6.Message
+
+[<CLIMutable>]
+type Values =
+    {
+        value1: string
+        operation: string
+        value2:string
+    }
+
+let calculatorHandler: HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        let values = ctx.BindQueryString<Values>()
+        let args = [|values.value1; convertToOperation values.operation;values.value2|]
+        let parsedValues = parseCalcArguments args
+        let result =
+            match parsedValues with
+            | Ok ok -> Ok (ok |||> calculate)
+            | Error error -> Error error
+        let message = convertMessageToString (result , args)
+        match message with
+        | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
+        | Error error -> (setStatusCode 400 >=> text error) next ctx
--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -8,31 +8,10 @@ open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
-open Hw6.Calculator
-open Hw6.Parser
-open Hw6.Message
+open Hw6.Logic
 
-[<CLIMutable>]
-type Values =
-    {
-        value1: string
-        operation: string
-        value2:string
-    }
 
-let calculatorHandler: HttpHandler =
-    fun (next : HttpFunc) (ctx : HttpContext) ->
-        let values = ctx.BindQueryString<Values>()
-        let args = [|values.value1; convertToOperation values.operation;values.value2|]
-        let parsedValues = parseCalcArguments args
-        let result =
-            match parsedValues with
-            | Ok ok -> Ok (ok |||> calculate)
-            | Error error -> Error error
-        let message = convertMessageToString (result , args)
-        match message with
-        | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
-        | Error error -> (setStatusCode 400 >=> text error) next ctx
+
 
 let webApp =
     choose [
