f8424c1643ffa8cccf57af2a5868835d383aaaad
Zancrow20 <ildar1qweas@gmail.com>
2022-11-24 19:18:15 +0300

    [mod] refactoring CalcVisitorImpl

--- Homework11/Hw11/Services/CalculatorVisitor/CalculatorVisitorImpl.cs
+++ Homework11/Hw11/Services/CalculatorVisitor/CalculatorVisitorImpl.cs
@@ -6,21 +6,21 @@ namespace Hw11.Services.CalculatorVisitor;
 
 public class CalcVisitorImpl : ICalculatorVisitor
 {
-    private Dictionary<Expression,Lazy<Task<double>>> _lazy = new ();
+    private Dictionary<Expression,Lazy<Task<double>>> _lazy;
     public async Task<double> CalculatorVisit( 
         Dictionary<Expression, Expression[]> expressionsMap)
     {
         var root = expressionsMap.Keys.First();
+        _lazy = new Dictionary<Expression, Lazy<Task<double>>>();
         foreach (var (expression, doBefore) in expressionsMap)
             _lazy[expression] = new Lazy<Task<double>>(async () =>
             {
                 await Task.WhenAll(doBefore.Select(b => _lazy[b].Value));
                 await Task.Yield();
                 
-                return await CalculateExpressions(expression as dynamic);
+                return await CalculateExpression(expression as dynamic);
             });
         var value = await _lazy[root].Value;
-        _lazy.Clear();
         return value;
     }
 
@@ -37,23 +37,22 @@ public class CalcVisitorImpl : ICalculatorVisitor
         => rightNode != 0 ? leftNode / rightNode : 
             throw new DivideByZeroException(DivisionByZero);
 
-    private async Task<double> CalculateExpressions(Expression expression)
+    private async Task<double> CalculateExpression(ConstantExpression constantExpression)
+        => await Task.FromResult((double) constantExpression.Value!);
+
+    private async Task<double> CalculateExpression(BinaryExpression binaryExpression)
+    {
+        await Task.Delay(1000);
+        var leftNode = await _lazy[binaryExpression.Left].Value;
+        var rightNode = await _lazy[binaryExpression.Right].Value;
+            
+        return binaryExpression.NodeType switch 
         {
-            if (expression is ConstantExpression constantExpression) 
-                return await Task.FromResult((double) constantExpression.Value!);
-            
-            await Task.Delay(1000);
-            var binaryExpression = expression as BinaryExpression;
-            var leftNode = await _lazy[binaryExpression.Left].Value;
-            var rightNode = await _lazy[binaryExpression.Right].Value;
-            
-            return expression.NodeType switch 
-            {
-                ExpressionType.Add => Add(leftNode, rightNode),
-                ExpressionType.Subtract => Subtract(leftNode, rightNode),
-                ExpressionType.Multiply => Multiply(leftNode, rightNode),
-                ExpressionType.Divide => Divide(leftNode,rightNode),
-                _ => throw new NotImplementedException("calculator doesn't support this operation")
-            };
-        }
+            ExpressionType.Add => Add(leftNode, rightNode),
+            ExpressionType.Subtract => Subtract(leftNode, rightNode),
+            ExpressionType.Multiply => Multiply(leftNode, rightNode),
+            ExpressionType.Divide => Divide(leftNode,rightNode),
+            _ => throw new NotImplementedException("calculator doesn't support this operation")
+        };
+    }
 }
